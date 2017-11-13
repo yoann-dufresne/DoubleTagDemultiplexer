d51c95e9182cc7f7bb569f04ccdeda2bb43fa112
yoann-dufresne <yoann.dufresne0@gmail.com>
2017-11-13 16:23:09 +0100

    Add matrix comparison for IUPAC symbols

---
+++ IUPAC.cpp
@@ -0,0 +1,77 @@
+#include "IUPAC.hpp"
+
+using namespace std;
+
+const bool matrix[15][15] = {
+	//         A      C      M      G      R      S      V      T      W      Y      H      K      D      B      N
+	/* A */ {true , false, true , false, true , false, true , false, true , false, true , false, true , false, true }, // A
+	/* C */ {false, true , true , false, false, true , true , false, false, true , true , false, false, true , true }, // C
+	/* M */ {true , true , true , false, true , true , true , false, true , true , true , false, true , true , true }, // M
+	/* G */ {false, false, false, true , true , true , true , false, false, false, false, true , true , true , true }, // G
+	/* R */ {true , false, true , true , true , true , true , false, true , false, true , true , true , true , true }, // R
+	/* S */ {false, true , true , true , true , true , true , false, false, true , true , true , true , true , true }, // S
+	/* V */ {true , true , true , true , true , true , true , false, true , true , true , true , true , true , true }, // V
+	/* T */ {false, false, false, false, false, false, false, true , true , true , true , true , true , true , true }, // T
+	/* W */ {true , false, true , false, true , false, true , true , true , true , true , true , true , true , true }, // W
+	/* Y */ {false, true , true , false, false, true , true , true , true , true , true , true , true , true , true }, // Y
+	/* H */ {true , true , true , false, true , true , true , true , true , true , true , true , true , true , true }, // H
+	/* K */ {false, false, false, true , true , true , true , true , true , true , true , true , true , true , true }, // K
+	/* D */ {true , false, true , true , true , true , true , true , true , true , true , true , true , true , true }, // D
+	/* B */ {false, true , true , true , true , true , true , true , true , true , true , true , true , true , true }, // B
+	/* N */ {true , true , true , true , true , true , true , true , true , true , true , true , true , true , true }  // N
+	//         A      C      M      G      R      S      V      T      W      Y      H      K      D      B      N
+};
+
+uint iupac_2_uint (const char & c) {
+	switch (c) {
+		case 'A':
+			return 1;
+		case 'C':
+			return 2;
+		case 'M':
+			return 3;
+		case 'G':
+			return 4;
+		case 'R':
+			return 5;
+		case 'S':
+			return 6;
+		case 'V':
+			return 7;
+		case 'T':
+			return 8;
+		case 'W':
+			return 9;
+		case 'Y':
+			return 10;
+		case 'H':
+			return 11;
+		case 'K':
+			return 12;
+		case 'D':
+			return 13;
+		case 'B':
+			return 14;
+		case 'N':
+			return 15;
+		default:
+			return 0;
+	}
+	return 0;
+}
+
+bool iupac_comp(const char & a, const char & b) {
+	return matrix[iupac_2_uint(a)-1][iupac_2_uint(b)-1];
+}
+
+
+// int main () {
+// 	cout << iupac_comp('A', 'C') << endl;
+// 	cout << iupac_comp('A', 'M') << endl;
+// 	cout << iupac_comp('N', 'C') << endl;
+
+// 	if (iupac_comp('N', 'C'))
+// 		cout << "Yeah !" << endl;
+
+// 	return 0;
+// }
---
+++ IUPAC.hpp
@@ -0,0 +1,4 @@
+#include <iostream>
+
+uint iupac_2_uint (const char & c);
+bool iupac_comp(const char & a, const char & b);
