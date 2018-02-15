dcaaa16e9259c5440148bf36e71ba0d6d3609f61
yoann-dufresne <yoann.dufresne0@gmail.com>
2018-02-15 15:51:34 +0100

    add explicit include for std errors

--- IUPAC.cpp
+++ IUPAC.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <stdexcept>
 
 #include "IUPAC.hpp"
 
--- demultiplexing.cpp
+++ demultiplexing.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <stdexcept>
 
 #include "demultiplexing.hpp"
 #include "Parser.hpp"
